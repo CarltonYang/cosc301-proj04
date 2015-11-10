a829b429ff9760c1ba0694ba1caaecadfe57ce14
cosc301 <carltonyang@gmail.com>
2015-11-09 21:16:36 -0500

    added clone()

--- proc.c
+++ proc.c
@@ -464,3 +464,115 @@ procdump(void)
     cprintf("\n");
   }
 }
+
+
+// Create a new process copying p as the parent.
+// Sets up stack to return as if from system call.
+// Caller must set state of returned proc to RUNNABLE.
+int
+clone(int fcn, int arg, int stack)
+{
+  int i, pid;
+  struct proc *np;
+
+  // Allocate process.
+  if((np = allocproc()) == 0)
+    return -1;
+  /*
+  // Copy process state from p.
+  if((np->pgdir = copyuvm(proc->pgdir, proc->sz)) == 0){
+    kfree(np->kstack);
+    np->kstack = 0;
+    np->state = UNUSED;
+    return -1;
+  }
+  */
+  np->pgdir = proc->pgdir;
+  np->sz = proc->sz;
+  np->parent = proc;
+  *np->tf = *proc->tf;
+  //np->stack=stack;
+  // Clear %eax so that fork returns 0 in the child.
+  np->tf->eax = 0;
+
+  for(i = 0; i < NOFILE; i++)
+    if(proc->ofile[i])
+      np->ofile[i] = filedup(proc->ofile[i]);
+  np->cwd = idup(proc->cwd);
+
+  safestrcpy(np->name, proc->name, sizeof(proc->name));
+ 
+  pid = np->pid;
+  np->isthread= 1;
+  // lock to force the compiler to emit the np->state write last.
+  //acquire(&ptable.lock);
+  //np->state = RUNNABLE;
+  //release(&ptable.lock);
+  
+//temporary array to copy into the bottom of news tack 
+//for the thread(i.e.,to the high address in stack 
+//page,since the stack grows downward) 
+	uint ustack[2];
+	uint sp=stack+PGSIZE; 
+	ustack[0]=0xffffffff;//fakereturnPC
+	ustack[1]=arg;
+	sp-=8;//stack grows down by 2 ints/8 bytes 
+	if(copyout(np->pgdir,sp,ustack,8)<0){ 
+	//failed to copy bottom of stack into newtask
+		return-1; }
+	np->tf->eip=fcn; 
+	np->tf->esp=sp; 
+	switchuvm(np); 
+	np->state=RUNNABLE;
+  return pid;
+}
+
+
+int join(int pid)
+{
+  if (proc->isthread ==1)
+    return -1;
+  if (proc->pid ==pid)
+    return -1;
+
+  struct proc *p;
+  int havekids ;//, pid;
+
+  acquire(&ptable.lock);
+  for(;;){
+    // Scan through table looking for zombie children.
+    havekids = 0;
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->parent != proc)
+        continue;
+      if (p-> isthread !=1)
+	continue;
+      havekids = 1;
+      if(p->state == ZOMBIE){
+        // Found one.
+        pid = p->pid;
+        kfree(p->kstack);
+        p->kstack = 0;
+        freevm(p->pgdir);
+        p->state = UNUSED;
+        p->pid = 0;
+        p->parent = 0;
+        p->name[0] = 0;
+        p->killed = 0;
+        release(&ptable.lock);
+        return pid;
+      }
+    }
+
+    // No point waiting if we don't have any children.
+    if(!havekids || proc->killed){
+      release(&ptable.lock);
+      return -1;
+    }
+
+    // Wait for children to exit.  (See wakeup1 call in proc_exit.)
+    sleep(proc, &ptable.lock);  //DOC: wait-sleep
+  }
+
+}
+
